59d4346e53ff1dd8ce636ab741ea77e67ae9adc8
anupmannem <anupmannem@gmail.com>
2019-04-29 03:31:54 +0530

    live code share feature with ot library setup

--- CodeShare/socket-server.js
+++ CodeShare/socket-server.js
@@ -1,10 +1,31 @@
 var socketIO = require('socket.io');
+var ot = require('ot');
+
+var roomList = {};
 
 module.exports = function(server) {
+	var str = 'Welcome to CodeShare code editor \n\n' + 
+				'console.log("Hello World!!")';
 	var io = socketIO(server);
 	io.on('connection', function(socket) {
+		socket.on('joinRoom', function(data) {
+			if (!roomList[data.room]) {
+				var socketIOServer = new ot.EditorSocketIOServer(str, [], data.room, function(socket, cb) {
+					cb(true);
+				});
+				roomList[data.room] = socketIOServer;
+			}
+			roomList[data.room].addClient(socket);
+			roomList[data.room].setName(socket, data.username);
+			
+			socket.room = data.room;
+			socket.join(data.room);
+		})
 		socket.on('chatMessage', function(data) {
-			io.emit('chatMessage', data);
+			io.to(socket.room).emit('chatMessage', data);
+		})
+		socket.on('disconnect', function() {
+			socket.leave(socket.room);
 		})
 	})
 }
