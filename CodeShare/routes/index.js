21bdeb87a4293a3e7ea190fab4aef736a3606718
anupmannem <anupmannem@gmail.com>
2019-04-28 18:00:31 +0530

    contact form setup

--- CodeShare/routes/index.js
+++ CodeShare/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Code-share' });
 });
+
+router.get('/about', function(req, res, next) {
+	res.render('about', {title: 'code-share: a plaform for code collaboration.'});
+});
+
+router.route('/contact')
+	.get(function(req, res, next) {
+		res.render('contact', {title: 'code-share: a platform for code collaboration.'})
+	})
+	.post(function(req, res, next) {
+		res.render('thank', {title: 'code-share: a platform for sharing code.'})
+	})
 
 module.exports = router;
