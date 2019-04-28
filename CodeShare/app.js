d9221df43f4a93759b89bb7e895c75a8bf504a0d
anupmannem <anupmannem@gmail.com>
2019-04-28 18:26:00 +0530

    contact form validation

--- CodeShare/app.js
+++ CodeShare/app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -14,8 +16,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- CodeShare/routes/index.js
+++ CodeShare/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Code-share' });
+  res.render('index', { title: 'Code-share', description: 'the platform for code collaboration' });
 });
 
 router.get('/about', function(req, res, next) {
@@ -15,7 +15,22 @@ router.route('/contact')
 		res.render('contact', {title: 'code-share: a platform for code collaboration.'})
 	})
 	.post(function(req, res, next) {
-		res.render('thank', {title: 'code-share: a platform for sharing code.'})
-	})
+		req.checkBody('name', 'Empty name').notEmpty();
+		req.checkBody('email', 'Invalid email').notEmpty();
+		req.checkBody('message', 'Empty message').notEmpty();
+		var errors = req.validationErrors();
+		
+		if(errors) {
+			res.render('contact', {
+				title: 'code-share: a plaform for code collaboration.',
+				name: req.body.name,
+				email: req.body.email,
+				message: req.body.messge,
+				errorMessages: errors
+			});
+		} else {
+			res.render('thank', {title: 'code-share: a platform for sharing code.'})	
+		}
+	});
 
 module.exports = router;
